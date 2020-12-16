22b6ca0c26f307ceada1972219d96377154f5986
David Qiu <davidqiu1997@hotmail.com>
2020-12-17 00:59:17 +1300

    Updates

---
+++ precache-manifest.7b0cc8957410a25e60ee805d6980ef66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52686f69fc94a8a1eb47ee4e75e637a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e76ac6467319604d651d",
+    "url": "/static/css/main.1c257721.chunk.css"
+  },
+  {
+    "revision": "3594dfa9901dc4c69a10",
+    "url": "/static/js/2.0c45e163.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0c45e163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76ac6467319604d651d",
+    "url": "/static/js/main.871f97be.chunk.js"
+  },
+  {
+    "revision": "77785b5d7f106e80a267",
+    "url": "/static/js/runtime-main.5bebf6b0.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
