829091dfc2efc1f166a9458fc5dfa03710e2f44f
David Qiu <davidqiu1997@hotmail.com>
2020-12-17 00:50:54 +1300

    Update

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './BusinessList.css';
-import Business from '../Business/Business';
+import React from "react";
+import "./BusinessList.css";
+import Business from "../business";
 
 class BusinessList extends React.Component {
-    render() {
-        return (
-            <div className="BusinessList">
-                <Businesss /> 
-                <Businesss />
-                <Businesss />
-                <Businesss />
-                <Businesss />
-                <Businesss />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
 }
 
-export default BusinessList
+export default BusinessList;
